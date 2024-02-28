9812e07b5403d72071d7bf2a0bf8d3adcf6e365a
Belhassen <152809495+BelhassenB@users.noreply.github.com>
2024-02-28 19:03:48 +0100

    add buttons to UI

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+let playerScore = 0;
+let computerScore = 0;
+
+const paperBtn = document.createElement('button')
+paperBtn.textContent = "Paper"
+const rockBtn = document.createElement('button')
+rockBtn.textContent = "Rock"
+const scissorBtn = document.createElement('button')
+scissorBtn.textContent = "Scissors"
+const div1 = document.createElement('div')
+div1.style.border = "2px solid black"
+div1.style.height = "200px"
+div1.style.textAlign = "center"
+
+const divPlayerScore = document.createElement('div')
+divPlayerScore.textContent = "Player Score : " 
+
+const divComputerScore = document.createElement('div')
+divComputerScore.textContent = "Computer Score : "
+
+
+const div2 = document.createElement('div')
+
+div2.style.height = "100px"
+div2.style.textAlign = "center"
+
+
+const body = document.querySelector('body')
+
+body.appendChild(paperBtn)
+body.appendChild(rockBtn)
+body.appendChild(scissorBtn)
+body.appendChild(div1)
+div1.appendChild(divPlayerScore)
+div1.appendChild(divComputerScore)
+body.appendChild(div2)
+
+const allButtons = document.querySelectorAll('button')
+
+allButtons.forEach(btn => {
+    btn.addEventListener('click', (e) =>{
+        const playerChoice = e.target.textContent   
+        playRound(`${playerChoice}`, getComputerChoice())       
+
+        
+
+    })
+})  
+
+
 //Create function that will randomly return Rock, Paper or Scissors
 function getComputerChoice () {
 
@@ -13,47 +63,37 @@ function getComputerChoice () {
     
 }
 
-//Create function that will ask for the player choice (Case Sensitive)
-function getPlayerChoice () {
-
-    const playerInput = prompt("Make your choice: Rock, Paper or Scissors", "")  
-    const playerInputLowC = (playerInput.slice(1)).toLowerCase();
-    const playerChoice = playerInput[0].toUpperCase() + playerInputLowC;
-
-        return playerChoice;
-}
-
-let playerScore = 0;
-let computerScore = 0;
-
+//Create function to start a game and compare the computer selection with the player selection
+function playRound (playerSelection, computerSelection) {     
     
-//Create function to start a game and compare the computer seelction with the player selection
-function playRound (playerSelection, computerSelection) {       
     
-    if (playerSelection === "Rock" && computerSelection === "Paper" ||
-        playerSelection === "Paper" && computerSelection === "Scissors" || 
-        playerSelection === "Scissors" && computerSelection === "Rock" ) {
-            computerScore++; return `Computer Choice: ${computerSelection}, Your choice is: ${playerSelection}, You Lost!\n
-            Computer Score: ${computerScore}`           
-
-    } else if (playerSelection === computerSelection ) {        
-        return `It\'s a tie! Computer Choice is: ${computerSelection}, Your choice is: ${playerSelection}`; 
-
-    } else {
-        playerScore++; return `Computer Choice is: ${computerSelection}, Your choice is: ${playerSelection} You Won! \n
-        Player Score: ${playerScore}`;
-        
+    while (computerScore < 5 && playerScore < 5) {
+        if (playerSelection === "Rock" && computerSelection === "Paper" ||
+            playerSelection === "Paper" && computerSelection === "Scissors" || 
+            playerSelection === "Scissors" && computerSelection === "Rock" ) {
+            divComputerScore.textContent = "Computer Score : " + ++computerScore;
+            return div2.textContent = `Computer Choice is: ${computerSelection}, Your choice is: ${playerSelection} You lost!`       
+        } else if (playerSelection === computerSelection ) {        
+        return div2.textContent = `It\'s a tie! Computer Choice is: ${computerSelection}, Your choice is: ${playerSelection}`
+        } else {
+        divPlayerScore.textContent = "Player Score : " + ++playerScore;
+        return div2.textContent = `Computer Choice is: ${computerSelection}, Your choice is: ${playerSelection} You Won!`
+        }
     }
-}
 
-//Create function to play severeal rounds based on a loop
-function game () {    
+    if (playerScore > computerScore) {
+        div2.style.color = "green"
+        div2.style.fontWeight = "bold"
+        div2.style.fontSize = "22px"
+        return div2.textContent = `You won ! Final score is ${playerScore}:${computerScore}`
+    }
+    else {
+        div2.style.color = "red"
+        div2.style.fontWeight = "bold"
+        div2.style.fontSize = "22px"
+        return div2.textContent = `You lost ! Final score is ${playerScore}:${computerScore}`
+    }
 
-    for (let i=0; i < 5; i++) 
-    console.log(playRound(getPlayerChoice(),getComputerChoice()));
 
-    let finalScore = (playerScore > computerScore)? `You won: ${playerScore} to ${computerScore}`: `You lost: ${playerScore} to ${computerScore}`
-    alert(finalScore);
-
+    
 } 
-game();
